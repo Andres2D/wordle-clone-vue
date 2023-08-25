8a8b4337b280145a3d40ae1eb5fd809256cabe31
Andres2D <andres2d1997.1206@gmail.com>
2023-08-24 19:46:32 -0500

    Add grid and input components

---
+++ src/components/GridComponent.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import InputComponent from './InputComponent.vue';
+import { gridWords } from '../composables/useState';
+</script>
+<template>
+  <container class="grid">
+    <div 
+      class="row"
+      v-for="(_, i) in gridWords"
+      :key="`row-${i}`"
+    >
+      <InputComponent 
+        v-for="(_, index) in ['','','','','']"
+        :word="gridWords[i][index]"
+        :key="`column-${index}`"
+      />
+    </div>
+  </container>
+</template>  
+<style lang="scss">
+.grid {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+
+  .row {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 15px;
+  }
+}
+</style>
---
+++ src/components/InputComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+  word: {
+    type: String,
+  }
+})
+</script>
+<template>
+  <div class="box">{{ word }}</div>
+</template>  
+<style lang="scss">
+.box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid gray;
+  text-align: center;
+  width: 65px;
+  min-height: 65px;
+  font-size: 25px;
+}
+</style>
